635cfa7d998bd64e1be7c0f75f18e718130da5f6
Alex Pererodov <pererodovsasha2362000@gmail.com>
2024-04-23 00:07:02 +0300

    add adress server

--- src/action/filesAction.js
+++ src/action/filesAction.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api/images/"
+const API_URL = "https://image-converter-backend-a5ig.onrender.com/api/images/"
 
 export const filesUpload = async (files) =>  {
     try {
